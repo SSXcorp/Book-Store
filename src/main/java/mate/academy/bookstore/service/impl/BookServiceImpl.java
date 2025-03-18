79e069d263d04493e89c2ff38f3c94db84685565
ssx_corp <mr.dimov77@gmail.com>
2025-03-18 11:04:27 +0200

    All required files added to GitHub PR

--- src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
@@ -1,1 +1,28 @@
+package mate.academy.bookstore.service.impl;
 
+import java.util.List;
+import mate.academy.bookstore.model.Book;
+import mate.academy.bookstore.repository.BookRepository;
+import mate.academy.bookstore.service.BookService;
+
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        List<Book> books = bookRepository.findAll();
+        if (books.isEmpty()) {
+            throw new RuntimeException("No books found");
+        }
+        return books;
+    }
+}
