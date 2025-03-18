f3134928e6a52cf0407b19ae207e4feafb4dd80e
ssx_corp <mr.dimov77@gmail.com>
2025-03-18 11:50:23 +0200

    Added config + test at main

--- src/main/java/mate/academy/bookstore/BookStoreApplication.java
+++ src/main/java/mate/academy/bookstore/BookStoreApplication.java
@@ -1,11 +1,37 @@
 package mate.academy.bookstore;
 
+import java.math.BigDecimal;
+import mate.academy.bookstore.model.Book;
+import mate.academy.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
+
+    @Autowired
+    private BookService bookService;
+
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book1 = new Book();
+            book1.setTitle("New York Times");
+            book1.setAuthor("James Bond");
+            book1.setPrice(BigDecimal.TEN);
+            book1.setCoverImage("d32iqd912ur4u3");
+            book1.setIsbn("isbn-1-test");
+
+            bookService.save(book1);
+
+            System.out.println(bookService.findAll());
+        };
     }
 }
